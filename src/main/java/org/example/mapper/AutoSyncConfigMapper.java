29a63f4fefcab00e2fc2560e484b577125f72dcb
한성재 <anis15740@gmail.com>
2026-03-17 17:29:56 +0900

    Fix: AutoSyncConfigMapper 오류 수정
    indexInfoId와 indexClassification 매핑 누락된 부분 추가

--- src/main/java/org/example/mapper/AutoSyncConfigMapper.java
+++ src/main/java/org/example/mapper/AutoSyncConfigMapper.java
@@ -3,8 +3,11 @@ package org.example.mapper;
 import org.example.dto.data.AutoSyncConfigDto;
 import org.example.entity.AutoSyncConfig;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AutoSyncConfigMapper {
+  @Mapping(source = "indexInfo.id", target = "indexInfoId")
+  @Mapping(source = "indexInfo.categoryName", target = "indexClassification")
   AutoSyncConfigDto toDto(AutoSyncConfig autoSyncConfig);
 }
